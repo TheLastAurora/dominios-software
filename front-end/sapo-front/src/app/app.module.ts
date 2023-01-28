7febb24f1a2af8862d65dbba5079c124cc3bd227
Robert-Martins <robertcantares@gmail.com>
2023-01-28 00:29:53 -0300

    apps-init

---
+++ front-end/sapo-front/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { ComponentsModule } from './components/components.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ComponentsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
