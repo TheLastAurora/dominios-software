47422ae4a82a7c60af8182db963223df3465abfc
Robert-Martins <robertcantares@gmail.com>
2023-02-19 16:29:08 -0300

    feat/list-component-implement

--- front-end/sapo-front/src/app/components/lists/lists.module.ts
+++ front-end/sapo-front/src/app/components/lists/lists.module.ts
@@ -2,16 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PublicListComponent } from './public-list/public-list.component';
 import { AdminListComponent } from './admin-list/admin-list.component';
+import { AdminListOptionComponent } from './admin-list-option/admin-list-option.component';
 
 
 
 @NgModule({
   declarations: [
     PublicListComponent,
-    AdminListComponent
+    AdminListComponent,
+    AdminListOptionComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    PublicListComponent,
+    AdminListComponent,
+    AdminListOptionComponent
   ]
 })
 export class ListsModule { }
