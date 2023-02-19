129b8e6df6213567f9939550db04ed936f8221ef
Robert-Martins <robertcantares@gmail.com>
2023-02-18 22:54:15 -0300

    bugfix/preflight-not-setted-header-option

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -20,12 +20,13 @@ export class App {
     private middlewares(): void {
         this.express.use(express.json());
         this.express.use((req: Request, res: Response, next: NextFunction) => {
-            res.setHeader("Access-Control-Allow-Origin", "*");
-            res.setHeader("Access-Control-Allow-Credentials", "true");
-            res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-            res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Credentials", "true");
+            res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+            res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
 
             this.express.use(cors());
+            this.express.options('*', cors());
             next();
         })
     }
