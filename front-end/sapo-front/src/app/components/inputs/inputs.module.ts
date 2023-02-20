d694e3e6827484120c334001ac06d6f792d1b356
Robert-Martins <robertcantares@gmail.com>
2023-02-20 09:31:53 -0300

    feat/concursos-tabs-nav

--- front-end/sapo-front/src/app/components/inputs/inputs.module.ts
+++ front-end/sapo-front/src/app/components/inputs/inputs.module.ts
@@ -2,16 +2,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TextInputComponent } from './text-input/text-input.component';
-import { SelectInputComponent } from './select-input/select-input.component';
-import { DateInputComponent } from './date-input/date-input.component';
+import { InputComponent } from './input/input.component';
 
 
 
 @NgModule({
   declarations: [
     TextInputComponent,
-    SelectInputComponent,
-    DateInputComponent
+    InputComponent
   ],
   imports: [
     CommonModule,
@@ -19,8 +17,7 @@ import { DateInputComponent } from './date-input/date-input.component';
   ],
   exports: [
     TextInputComponent,
-    SelectInputComponent,
-    DateInputComponent
+    InputComponent
   ]
 })
 export class InputsModule { }
